2376cf3222fa3e30cba0848cee643d7bdf846234
John Carlo Diaz <59531206+JCDiaz27@users.noreply.github.com>
2026-06-04 19:20:02 +0800

    Update script.js
    Changed the backend credentials

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
  * so the page always works locally too.
  */
 
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxzL_xq5bG_3wBVTmXW_cFN7q1PAx8NtS5kbmjqz5JW5MzKcaYdCAb2eSYULw8Iy_5y/exec'; // ← paste your Web App URL here after setup
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbycQR7ARFuZZTxfV5G5hbk9uE0pXrxPTAJrhbu4zSWhKbRvY7L1T1pe2Skt1Pvz6Tv2vQ/exec'; // ← paste your Web App URL here after setup
 
 // ─────────────────────────────────────────
 // DOM REFERENCES
